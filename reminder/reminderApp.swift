bb2d4c66213174e870d1a3a40b502eb905f81da9
Jay Unewal <jayunewal@outlook.com>
2022-07-26 10:26:18 +0530

    Initial Commit

---
+++ reminder/reminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  reminderApp.swift
+//  reminder
+//
+//  Created by Jay Unewal on 26/07/22.
+//
+
+import SwiftUI
+
+@main
+struct reminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
